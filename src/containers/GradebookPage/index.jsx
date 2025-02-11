12392a8f8db3de12f5a2dade5fa2a546235a142c
Cormilo <gitgitgithub@yandex.ru>
2025-02-11 19:32:08 +0300

    change body size 2

--- src/containers/GradebookPage/index.jsx
+++ src/containers/GradebookPage/index.jsx
@@ -51,7 +51,7 @@ export class GradebookPage extends React.Component {
       <WithSidebar
         sidebar={<GradebookFilters updateQueryParams={this.updateQueryParams} />}
       >
-        <div className="px-3 gradebook-content">
+        <div className="px-3 gradebook-content" style={{ minHeight: '80vh' }}>
           <GradebookHeader />
           {(this.props.activeView === views.bulkManagementHistory
             ? <BulkManagementHistoryView />
