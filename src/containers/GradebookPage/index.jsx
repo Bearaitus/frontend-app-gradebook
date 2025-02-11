9a8b4882b50f623136de0bcb5710acd95a01d4d1
Cormilo <gitgitgithub@yandex.ru>
2025-02-11 19:50:37 +0300

    remove gradebook size 2

--- src/containers/GradebookPage/index.jsx
+++ src/containers/GradebookPage/index.jsx
@@ -51,7 +51,7 @@ export class GradebookPage extends React.Component {
       <WithSidebar
         sidebar={<GradebookFilters updateQueryParams={this.updateQueryParams} />}
       >
-        <div className="px-3 gradebook-content" style={{ minHeight: '80vh' }}>
+        <div className="px-3 gradebook-content">
           <GradebookHeader />
           {(this.props.activeView === views.bulkManagementHistory
             ? <BulkManagementHistoryView />
