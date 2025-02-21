4a4b07c4a973debaf9101740c5169548670dec6f
guardinrgb <guardin302@gmail.com>
2025-02-21 18:38:48 +0300

    showing student

--- src/components/GradesView/FilteredUsersLabel/messages.js
+++ src/components/GradesView/FilteredUsersLabel/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   visibilityLabel: {
     id: 'gradebook.GradesTab.usersVisibilityLabel',
-    defaultMessage: 'Showing {filteredUsers} total students',
+    defaultMessage: 'Всего студентов: {filteredUsers}',
     description: 'Users visibility label',
   },
 });
