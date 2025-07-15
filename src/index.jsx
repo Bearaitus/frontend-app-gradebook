46c6dacf0a2d316ff8d2d99273a93a5880da4f79
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-15 11:45:54 +0300

    вот так лучше

--- src/index.jsx
+++ src/index.jsx
@@ -11,11 +11,15 @@ import {
   subscribe,
 } from '@edx/frontend-platform';
 
+// import lightning from './lightning';
 
 import messages from './i18n';
 import App from './App';
 
-
+subscribe(APP_READY, () => {
+  // lightning();
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
 
 initialize({
   handlers: {
